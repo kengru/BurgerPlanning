71fa143be5ed0afef0139e32282aea516810b420
Kendry Grullon <kengrullon@gmail.com>
2018-12-28 16:33:09 -0400

    Added the main burger builder component.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import classes from './Layout.css';
+
 const layout = (props) => {
   return (
     <>
       <div>Toolbar, SideDrawer, Backdrop</div>
-      <main>
+      <main className={classes.Content}>
         {props.children}
       </main>
     </>
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 
 import Layout from '../components/Layout/Layout';
+import BurgerBuilder from './BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <p> Working! </p>
+          <BurgerBuilder></BurgerBuilder>
         </Layout>
       </div>
     );
