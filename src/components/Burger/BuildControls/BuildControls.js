7a8de689e91442b2a2c6330ee998f0158195575c
Kendry Grullon <kengrullon@gmail.com>
2019-01-16 15:17:41 -0400

    Added proptypes and dynamic building out of ingredients object.

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import classes from './BuildControls.css';
+
+const buildControls = (props) => {
+  return () {
+    <div className={classes.BuildControls}>
+      jss
+    </div>
+  }
+};
+
+export default buildControls;
--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,1 +1,29 @@
 import React from 'react';
+
+import classes from './Burger.css';
+import BurgerIngridient from './BurgerIngredient/BurgerIngredient';
+
+const burger = (props) => {
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngridient key={igKey + i} type={igKey}/>
+      }); // [,]
+    }).reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please add ingredients!</p>
+  }
+  return (
+    <div>
+      <div className={classes.Burger}>
+        <BurgerIngridient type="bread-top"/>
+        {transformedIngredients}
+        <BurgerIngridient type="bread-bottom"/>
+      </div>
+    </div>
+  );
+};
+
+export default burger;
