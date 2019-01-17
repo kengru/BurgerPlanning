1c393877ed387659264fdd2382a2081a23be645e
Kendry Grullon <kengrullon@gmail.com>
2019-01-17 14:15:11 -0400

    Checking for state data to disable buttons.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -54,10 +54,17 @@ class BurgerBuilder extends Component {
   }
 
   render () {
+    const disabledInfo = {
+      ...this.state.ingredients
+    };
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0;
+    }
     return (
       <>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls 
+          disabled={disabledInfo}
           ingredientAdded={this.addIngredientHandler}
           ingredientRemoved={this.removeIngredientHandler}/>
         <button>Checkout</button>
