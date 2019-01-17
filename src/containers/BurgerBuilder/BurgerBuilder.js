5b948fb5488d19a8041faf104ed100e174878550
Kendry Grullon <kengrullon@gmail.com>
2019-01-17 14:04:06 -0400

    Added the build controls and manipulation to the state via them.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7
+}
 
 class BurgerBuilder extends Component {
   // constructor(props) {
@@ -10,18 +18,48 @@ class BurgerBuilder extends Component {
 
   state = {
     ingredients: {
-      salad: 1,
+      salad: 0,
       bacon: 1,
       cheese: 1,
       meat: 1,
+    },
+    totalPrice: 5
+  }
+
+  addIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+    updatedIngredients[type] = oldCount + 1;
+    const priceAddition = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAddition;
+    this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+  }
+
+  removeIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    if (oldCount <= 0){
+      return;
     }
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+    updatedIngredients[type] = oldCount - 1;
+    const priceSubstraction = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - priceSubstraction;
+    this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
   }
 
   render () {
     return (
       <>
         <Burger ingredients={this.state.ingredients} />
-        <div>Build Controls</div>
+        <BuildControls 
+          ingredientAdded={this.addIngredientHandler}
+          ingredientRemoved={this.removeIngredientHandler}/>
         <button>Checkout</button>
       </>
     );
