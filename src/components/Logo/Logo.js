1660590a5bae65dc59b5cd71102b95ded6e5de81
Kendry Grullon <kengrullon@gmail.com>
2019-01-24 14:19:48 -0400

    SideDrawer
    Creating a sidedrawer for mobile view.

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,7 +4,7 @@ import burgerLogo from '../../assets/images/burger-logo.png';
 import classes from './Logo.css';
 
 const logo = (props) => (
-  <div className={classes.Logo}>
+  <div className={classes.Logo} style={{height: props.height}}>
     <img alt="The last logo." src={burgerLogo}/>
   </div>
 );
