b207c7f4e3007a35e8cb393ec1e2e909a7b28908
Kendry Grullon <kengrullon@gmail.com>
2019-01-24 15:28:47 -0400

    Styling SD css
    Styling the side drawer css, also adding classes dinamically to close the drawer.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => {
-  return (
-    <>
-      <Toolbar/>
-      <SideDrawer/>
-      <main className={classes.Content}>
-        {props.children}
-      </main>
-    </>
-  )
+class Layout extends Component {
+  state = {
+    showSD: true
+  }
+
+  sideDCHandler = () => {
+    this.setState({ showSD: false });
+  }
+
+  render () {
+    return (
+      <>
+        <Toolbar />
+        <SideDrawer open={this.state.showSD} closed={this.sideDCHandler} />
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </>
+    )
+  }
 };
 
-export default layout;
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -6,11 +6,16 @@ import NavigationItems from '../NavigationItems/NavigationItems';
 import Backdrop from '../../UI/Backdrop/Backdrop';
 
 const sideDrawer = (props) => {
-    
+  let attClasses = [classes.SideDrawer, classes.Close];
+
+  if (props.open) {
+    attClasses = [classes.SideDrawer, classes.Open];
+  }
+
   return (
     <>
-      <Backdrop show/>
-      <div className={classes.SideDrawer}>
+      <Backdrop show={props.open} clicked={props.closed}/>
+      <div className={attClasses.join(' ')}>
         <div className={classes.Logo}>
           <Logo/>
         </div>
