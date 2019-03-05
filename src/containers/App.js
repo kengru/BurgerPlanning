473f791541842eb74be5f9712483537cd16e9956
Kendry Grullon <kengrullon@gmail.com>
2019-03-05 13:53:03 -0400

    Added a logout container to handle logout and redirection.

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,8 +4,9 @@ import { Route, Switch } from "react-router-dom";
 import Layout from "../hoc/Layout/Layout";
 import BurgerBuilder from "./BurgerBuilder/BurgerBuilder";
 import Checkout from "./Checkout/Checkout";
-import Orders from "../containers/Orders/Orders";
-import Auth from "../containers/Auth/Auth";
+import Orders from "./Orders/Orders";
+import Auth from "./Auth/Auth";
+import Logout from "./Auth/Logout/Logout";
 
 class App extends Component {
   render() {
@@ -16,6 +17,7 @@ class App extends Component {
             <Route path="/orders" component={Orders} />
             <Route path="/checkout" component={Checkout} />
             <Route path="/auth" component={Auth} />
+            <Route path="/logout" component={Logout} />
             <Route path="/" exact component={BurgerBuilder} />
           </Switch>
         </Layout>
