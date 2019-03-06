d57672cf91cdc9c4bd5f80d4649fa194d23260cb
Kendry Grullon <kengrullon@gmail.com>
2019-03-06 08:27:13 -0400

    Added lazy loading, not gaining much but practice, which is ok.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from "react";
 import { Route, Switch, withRouter, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
+import asyncComponent from "../hoc/asyncComponent/asyncComponent"
 
 import Layout from "../hoc/Layout/Layout";
 import BurgerBuilder from "./BurgerBuilder/BurgerBuilder";
-import Checkout from "./Checkout/Checkout";
-import Orders from "./Orders/Orders";
-import Auth from "./Auth/Auth";
 import Logout from "./Auth/Logout/Logout";
 import * as actions from "../store/actions/index";
+
+const asyncCheckout = asyncComponent(() => {
+  return import("./Checkout/Checkout");
+})
+
+const asyncOrders = asyncComponent(() => {
+  return import("./Orders/Orders");
+})
+
+const asyncAuth = asyncComponent(() => {
+  return import("./Auth/Auth");
+})
 
 class App extends Component {
   componentDidMount() {
@@ -18,7 +28,7 @@ class App extends Component {
   render() {
     let routes = (
       <Switch>
-        <Route path="/auth" component={Auth} />
+        <Route path="/auth" component={asyncAuth} />
         <Route path="/" exact component={BurgerBuilder} />
         <Redirect to="/"/>
       </Switch>
@@ -26,9 +36,9 @@ class App extends Component {
     if (this.props.isAuth) {
       routes = (
         <Switch>
-          <Route path="/orders" component={Orders} />
-          <Route path="/checkout" component={Checkout} />
-          <Route path="/auth" component={Auth} />
+          <Route path="/orders" component={asyncOrders} />
+          <Route path="/checkout" component={asyncCheckout} />
+          <Route path="/auth" component={asyncAuth} />
           <Route path="/logout" component={Logout} />
           <Route path="/" exact component={BurgerBuilder} />
         </Switch>
